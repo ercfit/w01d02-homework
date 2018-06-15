d67ef5fefd3da72f6104410c39e33e83cf4c3256
Erica Bruenton <bosslady@Ericas-MacBook-Air.local>
2018-06-15 16:42:07 -0600

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,40 @@
+//1. 
+// console.log("what is broken now?");
+
+//2.
+// for (i = 0; i < 100; i++){
+// 	console.log(i);
+// }
+
+//3.
+
+// for (i = 99; i >= 0; i--){
+// 	console.log(i);
+// }
+
+//4.
+// for(i = 0; i < 100; i += 2){
+// 	console.log(i);
+// }
+
+//5.
+// for(i = 98; i >= 0; i -=2){
+// 	console.log(i);
+// }
+
+//6.
+// for(i = 49; i < 73; i++){
+// console.log(i);
+// }
+
+//7.
+// for(i = 81; i >= 26; i--){
+// 	console.log(i);
+// }
+
+//8.
+// for(i = 3; i < 91; i++){
+// 	if (i % 3 == 0){
+// 	console.log(i);
+// 	}
+// }
